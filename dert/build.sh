c93e7171fbee61a0b015393d362f514e97da6b8b
lkeelyme <leslie.keely@nasa.gov>
2016-05-19 15:42:17 -0700

    Release 1.0b3.

--- dert/build.sh
+++ dert/build.sh
@@ -8,17 +8,24 @@ setenv JAVACMD /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/
 
 echo $JAVACMD
 
-set VERSION = "1.0b2"
+set DERT_VERSION = "1.0b3"
 
-ant -v -buildfile build_linux.xml clean -Dversion=$VERSION
-ant -v -buildfile build_linux.xml main -Dversion=$VERSION
+ant -v -buildfile build_linux.xml clean -Ddertversion=$DERT_VERSION
+ant -v -buildfile build_linux.xml main -Ddertversion=$DERT_VERSION
 
-ant -v -buildfile build_osx.xml clean -Dversion=$VERSION
-ant -v -buildfile build_osx.xml main -Dversion=$VERSION
+ant -v -buildfile build_osx.xml clean -Ddertversion=$DERT_VERSION
+ant -v -buildfile build_osx.xml main -Ddertversion=$DERT_VERSION
 
 # Set back to Java 1.8 to create the Mac app bundle.
 setenv JAVACMD
 
 echo $JAVACMD
 
-ant -v -buildfile build_macapp.xml -Dversion=$VERSION
+ant -v -buildfile build_macapp.xml -Ddertversion=$DERT_VERSION
+
+zip -r dert_${DERT_VERSION}_linux.zip dert_${DERT_VERSION}_linux
+chmod 644 dert_${DERT_VERSION}_linux.zip
+zip -r dert_${DERT_VERSION}_osx.zip dert_${DERT_VERSION}_osx
+chmod 644 dert_${DERT_VERSION}_osx.zip
+
+echo DERT build complete
