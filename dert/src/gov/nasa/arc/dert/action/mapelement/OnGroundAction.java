cc373e2637923faafe57ee605db43263070e01a7
lkeelyme <leslie.keely@nasa.gov>
2016-09-16 17:40:44 -0700

    Add menu action to put a map element on the ground and zero out its Z offset.

---
+++ dert/src/gov/nasa/arc/dert/action/mapelement/OnGroundAction.java
@@ -0,0 +1,30 @@
+package gov.nasa.arc.dert.action.mapelement;
+
+import gov.nasa.arc.dert.action.MenuItemAction;
+import gov.nasa.arc.dert.scene.MapElement;
+
+/**
+ * Context menu item for placing a map element at a point in the landscape. User
+ * is prompted with a list of map elements.
+ *
+ */
+public class OnGroundAction extends MenuItemAction {
+
+	protected MapElement mapElement;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param position
+	 */
+	public OnGroundAction(MapElement mapElement) {
+		super("Ground "+ mapElement.getName());
+		this.mapElement = mapElement;
+	}
+
+	@Override
+	protected void run() {
+		mapElement.ground();
+	}
+
+}
